1b80f387fb440aec3625b6752a536dc75fbefe2e
ICE-CBing <87920801+ICE-CBing@users.noreply.github.com>
2021-07-25 22:56:41 +0800

    Delete ice_text directory

--- ice_text/text2.py
+++
@@ -1,8 +0,0 @@
-print("2")
-<<<<<<< HEAD
-print("99999999")
-print("345")
-print("23333333333")
-=======
->>>>>>> parent of 04b02fc (Merge branch 'ice-text' of https://github.com/MoYuStudio/SUGT06 into ice-text)
-print("99999999")
