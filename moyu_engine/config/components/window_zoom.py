4c0abb847aa146ec26fd720fc2ab0d00ccdfb2f5
WilsonVinson <cprewilsonvinson@gmail.com>
2021-08-21 20:48:13 +0800

    main

--- moyu_engine/config/components/window_zoom.py
+++ moyu_engine/config/components/window_zoom.py
@@ -7,9 +7,13 @@ def zoom_Fn():
             C.game_main_surface_level = 10
         else:
             C.game_main_surface_level -= 1
+            C.move_y -= 4.25
+            C.move_x -= 7.9
     if C.zoom_out == True:
         if C.game_main_surface_level == 60:
             C.game_main_surface_level = 60
         else:
             C.game_main_surface_level += 1
+            C.move_y += 4.25
+            C.move_x += 7.9
     return C.game_main_surface_level
--- moyu_engine/config/event/game_main_event.py
+++ moyu_engine/config/event/game_main_event.py
@@ -11,22 +11,7 @@ import components.tilemap_manager
 import components.tilebutton
 import components.scrollbar
 
-def zoom_Fn():
-    if C.zoom_in == True:
-        if C.game_main_surface_level == 10:
-            C.game_main_surface_level = 10
-        else:
-            C.game_main_surface_level -= 1
-    if C.zoom_out == True:
-        if C.game_main_surface_level == 60:
-            C.game_main_surface_level = 60
-        else:
-            C.game_main_surface_level += 1
-    return C.game_main_surface_level
-
 def event():
-
-    zoom_Fn()
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -72,10 +57,10 @@ def event():
                 C.move_right = True
 
             if event.key == K_q:
-                C.zoom_in == True
+                C.zoom_in = True
 
             if event.key == K_e:
-                C.zoom_out == True
+                C.zoom_out = True
 
             if event.key == K_z:
                 C.game_main_surface_level = 10
@@ -98,3 +83,9 @@ def event():
 
             if event.key == K_RIGHT or event.key == K_d:
                 C.move_right = False
+
+            if event.key == K_q:
+                C.zoom_in = False
+
+            if event.key == K_e:
+                C.zoom_out = False
