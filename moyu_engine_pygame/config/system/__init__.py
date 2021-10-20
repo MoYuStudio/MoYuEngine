c78aa19993ceabaadadddc85f806ae890dfbe6fd
WilsonVinson <cprewilsonvinson@gmail.com>
2021-10-20 21:38:56 +0800

    main

--- moyu_engine_pygame/config/system/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from .AssetsSystem import AssetsSystem
-from .MoveSystem import MoveSystem
-from .PopupSystem import PopupSystem
-from .ThreadSystem import ThreadSystem
-from .TilemapSystem import TilemapSystem
-from .TransitionSystem import TransitionSystem
-from .WindowsSystem import WindowsSystem
