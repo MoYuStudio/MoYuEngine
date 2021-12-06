aeba7269900ad80bd558ba9f411a3dce8e7f9481
WilsonVinson <cprewilsonvinson@gmail.com>
2021-12-06 18:08:11 +0800

    main

---
+++ moyu_engine/components/ui.py
@@ -0,0 +1,17 @@
+
+import pygame
+
+class UI:
+    def __init__(self,config={
+
+                            }):
+        self.config = config
+        
+    def button(self):
+        button_ruct = pygame.Rect((10*1 + 64*0,WINDOW_SIZE[1] - (10*1 + 64*1),64,64),width=0)
+    def blit(self):
+        # button
+        pygame.Rect.collidepoint(C.GAMEbar_button01_RECT,event.pos)
+
+if __name__ == '__main__':
+    pass
