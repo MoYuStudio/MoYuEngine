00f4a532c1b08e2325cf6738ae12fa7ea27449b0
WilsonVinson <cprewilsonvinson@gmail.com>
2022-03-02 17:09:03 +0800

    main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 
-
 import moyu_engine
 
 window = moyu_engine.components.window.Window()
 surface = moyu_engine.components.surface.Surface()
 
-def suf():
+def page1():
 
     surface.surface.fill((255,55,55,255))
 
     surface.blit(window.screen)
 
-window.blit = suf
+window.blit = page1
 window.set()
+
+
 
