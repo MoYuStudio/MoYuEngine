e3bd1b1a665b8e01d35a52fd36a6bcd3a40b277d
WilsonVinson <cprewilsonvinson@gmail.com>
2023-06-05 17:51:20 +0800

    main

--- moyu_engine/main.py
+++ moyu_engine/main.py
@@ -7,9 +7,9 @@ WINDOW_HEIGHT = 1080
 TILE_WIDTH = 128
 TILE_HEIGHT = 128
 
-TILE_MAP_SIZE = 12
+TILE_MAP_SIZE = 16
 
-VIEW_SPEED = 0.1*TILE_MAP_SIZE
+VIEW_SPEED = 0.06*TILE_MAP_SIZE
 
 def main():
     pyray.init_window(WINDOW_WIDTH, WINDOW_HEIGHT, 'MoYuEngine')
@@ -32,13 +32,13 @@ def main():
             print('space')
 
         if pyray.is_key_down(pyray.KEY_W):
-            view_y -= VIEW_SPEED
-        if pyray.is_key_down(pyray.KEY_S):
             view_y += VIEW_SPEED
+        if pyray.is_key_down(pyray.KEY_S):
+            view_y -= VIEW_SPEED
         if pyray.is_key_down(pyray.KEY_A):
-            view_x -= VIEW_SPEED
-        if pyray.is_key_down(pyray.KEY_D):
             view_x += VIEW_SPEED
+        if pyray.is_key_down(pyray.KEY_D):
+            view_x -= VIEW_SPEED
             
         mouse_pos = pyray.get_mouse_position()
         mouse_tile_x = (mouse_pos.x - view_x) // (TILE_WIDTH // 2)
